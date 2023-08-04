278844d524e798f50367cb04c080555370a9f373
Sultanbek <sultan.sekenov@yandex.ru>
2023-08-04 17:13:18 +0600

    deleted commentaries

--- app/redux/store/store.tsx
+++ app/redux/store/store.tsx
@@ -3,42 +3,6 @@ import commentsReducer from "../comments/comments";
 import likeReducer from "../likes/likes";
 import checkReducer from "../check/check";
 import { save, load } from "redux-localstorage-simple";
-// export const store = configureStore({
-//   reducer: {
-//     comments: commentsReducer,
-//     likes: likeReducer,
-//   },
-// });
-
-// const saveToLocalStorage = (state: any) => {
-//   try {
-//     const serializedState = JSON.stringify(state);
-//     localStorage.setItem("state", serializedState);
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-
-// const loadFromLocalStorage = () => {
-//   try {
-//     const serializedState = localStorage.getItem("state");
-//     if (serializedState === null) return undefined;
-//     return JSON.parse(serializedState);
-//   } catch (e) {
-//     console.log(e);
-//     return undefined;
-//   }
-// };
-
-// const persistedState = loadFromLocalStorage();
-
-// export const store = configureStore({
-//   reducer: {
-//     comments: commentsReducer,
-//     likes: likeReducer,
-//   },
-//   preloadedState: persistedState,
-// });
 
 export const store = configureStore({
   reducer: {
@@ -48,12 +12,7 @@ export const store = configureStore({
   },
   preloadedState: load(),
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(save()),
-  //   middleware: getDefaultMiddleware().prepend(save()),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-// store.subscribe(() => saveToLocalStorage(store.getState()));
-
-// export default store;
