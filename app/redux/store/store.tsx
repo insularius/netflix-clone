7f60b26796e52309c3c5fa6dec75c42ab85772ec
Sultanbek <sultan.sekenov@yandex.ru>
2023-08-04 17:14:45 +0600

    minor change

--- app/redux/store/store.tsx
+++ app/redux/store/store.tsx
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import commentsReducer from "../comments/comments";
 import likeReducer from "../likes/likes";
 import checkReducer from "../check/check";
