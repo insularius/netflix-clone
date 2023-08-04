10d89c002a7057b64fb9aeab88ead35c5d208611
Sultanbek <sultan.sekenov@yandex.ru>
2023-08-04 17:16:57 +0600

    minor change

--- app/services/axiosInstance.ts
+++ app/services/axiosInstance.ts
@@ -11,23 +11,6 @@ import axios from "axios";
 import { getIdToken, refreshToken } from "../types/authProvide";
 import { RegisterResponse } from "../types/griffon";
 
-// const getIdToken = (): string | null => {
-//   try {
-//     const storedTokens = localStorage.getItem(TOKEN_STORAGE_KEY);
-//     if (!storedTokens) {
-//       console.log("No stored tokens found");
-//       return null;
-//     }
-//     const tokens: AuthTokens = JSON.parse(storedTokens);
-//     return tokens.id_token ? tokens.id_token : null;
-//   } catch (e) {
-//     console.error(e);
-//     return null;
-//   } finally {
-//     console.log(`getIdToken вызвался`);
-//   }
-// };
-
 const instance = axios.create({
   baseURL: BASE_GRIFFON_URL,
 });
@@ -45,20 +28,6 @@ instance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
-
-// instance.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-//     if (axios.isAxiosError(error) && error.response?.status === 401) {
-//       token = getIdToken();
-//       error.config.headers["Authorization"] = `Bearer ${token}`;
-//       return instance(error.config);
-//     }
-//     return Promise.reject(error);
-//   }
-// );
 
 instance.interceptors.response.use(
   (response) => {
@@ -84,5 +53,4 @@ instance.interceptors.response.use(
   }
 );
 
-// instance.interceptors.request.use();
 export default instance;
--- app/services/getCategoriesList.ts
+++ app/services/getCategoriesList.ts
@@ -7,35 +7,3 @@ export const getCategoriesList = () => {
     .get<CategoriesResponse>(`${CMS_ROOT}/video-categories`)
     .then((response) => response.data);
 };
-
-// import axios from "axios";
-// import { getProductListResponse } from "../types/getProductListResponse";
-// import qs from "qs";
-// const API_ROOT = "https://dario-cms.dar-dev.zone/api";
-
-// type Args = {
-//   categoryId?: number[];
-//   query?: string;
-// };
-// export const getProductsList = async ({ categoryId, query }: Args) => {
-//   const params = qs.stringify(
-//     {
-//       populate: ["image", "daru_category"],
-//       filters: {
-//         name: {
-//           $containsi: query,
-//         },
-//         daru_category: {
-//           id: {
-//             $in: categoryId,
-//           },
-//         },
-//       },
-//     },
-//     { encodeValuesOnly: true }
-//   );
-
-//   return await axios
-//     .get<getProductListResponse>(`${API_ROOT}/daru-products?${params}`)
-//     .then((res) => res.data);
-// };
